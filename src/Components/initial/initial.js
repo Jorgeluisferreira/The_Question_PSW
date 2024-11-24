df110b7c3ec273057e82a13cb838c34fd549875f
Jorge Luis <jorgejlfa@hotmail.com>
2024-11-24 12:55:11 -0300

    initial povisorio (apenas mobile)

--- src/Components/initial/initial.js
+++ src/Components/initial/initial.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './initial.css'; 
-import box from '../../images/box.png'
-import Button from '../button/button';
+import box from "../../images/box.png"
 
 const Initial = () => {
     return (
         <div className='initial'>
-            
+            <div className='up'>
+                <h2>Revele a verdade dentro da caixa</h2>
+            </div>
+
+            <div className='down'>
+                <i class="bi bi-chevron-compact-down arrow-down"></i>
+            </div>
         </div>
     );
 };
