410dbcf7dc567836f7f589c845a09d9141bdc6cf
Jorge Luis <jorgejlfa@hotmail.com>
2024-12-09 19:57:49 -0300

    hotfix

--- src/Pedidos.js
+++ src/Pedidos.js
@@ -44,7 +44,9 @@ function Pedidos(){
       <h2>Seus Pedidos</h2>
       <div className="list-group">
         {vendas.map((venda) => (
-          <div key={venda.id} className="list-group-item mb-3">
+          venda.idUser === currentUser.id ?
+          <>
+            <div key={venda.id} className="list-group-item mb-3">
             <div className="d-flex justify-content-between align-items-center">
               <div>
                 <h5>Pedido numero: {venda.id}</h5>
@@ -61,7 +63,7 @@ function Pedidos(){
             {expanded === venda.id && (
               <div className="mt-3">
                 <h6>Detalhes do Pedido:</h6>
-                <p>Tema da Caixa: {findBox(venda.idCaixa)}</p>
+                <p>ID do Caixa: {findBox(venda.idCaixa)}</p>
                 <p>
                   Atualizações:{" "}
                   {venda.mensagens
@@ -73,7 +75,10 @@ function Pedidos(){
                 </p>
               </div>
             )}
-          </div>
+          </div>   
+          </>
+          : <h4>não foram encontrados pedidos</h4>
+
         ))}
       </div>
     </div>
