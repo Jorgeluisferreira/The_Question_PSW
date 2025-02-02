ad6cf98d6cc8413a0c61cbf2fd8ed968fe588099
vivimraes <vitoria.morae123@gmail.com>
2025-02-02 01:19:05 -0300

    primeiros esquemas

--- backend/routes/boxes.js
+++ backend/routes/boxes.js
@@ -1,18 +1,48 @@
-var express = require('express');
-var router = express.Router();
-const Plans = require('../models/plans')
+const express = require('express');
+const router = express.Router();
+const Box = require('../models/Box');
 
-/* GET users listing. */
-router.route('/')
-.get((req, res, next) =>{
-  Plans.find({}).then((plans) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(plans);
-    console.log('get plans infos')
-  }, (err) => {console.log( 'error:'+ err)})
-  .catch((err) => {console.log('error outside'+err)})
+// Buscar todas as caixas
+router.get('/', async (req, res) => {
+  try {
+    const boxes = await Box.find();
+    res.json(boxes);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar caixas' });
+  }
+});
 
-})
+// Criar uma nova caixa
+router.post('/', async (req, res) => {
+  try {
+    const box = new Box(req.body);
+    await box.save();
+    res.status(201).json(box);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar caixa' });
+  }
+});
+
+// Atualizar uma caixa
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedBox = await Box.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedBox);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao atualizar caixa' });
+  }
+});
+
+// Deletar uma caixa
+router.delete('/:id', async (req, res) => {
+  try {
+    await Box.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Caixa deletada' });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao deletar caixa' });
+  }
+});
+
+module.exports = router;
 
 module.exports = router;
